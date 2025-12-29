8bf79f6ceb2dffdd0ec319a41a5a87790d7e58a7
Rashid Kabzhanov <ceo@bizdnai.com>
2025-12-29 08:56:28 +0000

    chore: remove 90+ temp scripts and old backup folder

--- backup_20251226_194526/bot/states.py
+++
@@ -1,6 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class SalesFlow(StatesGroup):
-    qualifying = State()
-    collecting_phone = State()
-    waiting_manager_message = State()  # NEW: waiting for message to manager
